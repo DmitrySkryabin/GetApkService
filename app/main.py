bde2a414cdf764220e59e86862d7d7ff6f028082
Dmitry SKryabin <skryabind98@gmail.com>
2025-03-20 18:33:41 +0900

    add creating apk_folder folder

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import config
+import os
 
 from fastapi import FastAPI
 from app.routers import apk_router
@@ -6,6 +7,15 @@ from app.routers import apk_router
 app = FastAPI()
 
 app.include_router(apk_router.router)
+
+'''
+Создаем папку где будем хранить Apk, если ее не существует
+'''
+if not os.path.exists(config.APK_FOLDER):
+    os.makedirs(config.APK_FOLDER)
+    print(f"Directory '{config.APK_FOLDER}' created.")
+else:
+    print(f"Directory '{config.APK_FOLDER}' already exists.")
 
 @app.get("/")
 def main():
